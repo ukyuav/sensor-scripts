281694c7134fa5e033f22b37cb307fca2c5c6ed2
justin-t12 <justin.t12@outlook.com>
2024-05-08 16:50:19 -0400

    updated how channel numbers are stored and iterated through

--- i2c_tca_2.lua
+++ i2c_tca_2.lua
@@ -10,7 +10,7 @@ Author: Ryan Prince | Last Updated By: Justin Tussey
 Last Updated: 07 May 2024
 
 ]]
-   --
+--
 -- init i2c bus
 local log_data = {}
 local i2c_bus = i2c:get_device(0, 0)
@@ -20,8 +20,13 @@ gcs:send_text(7, "i2c_tca Script Started!")
 -- var for address of the sensors
 local sensor_addr = 0x28
 
--- var for number of sensors on each multiplexer
-local sensor_ammount = 5
+-- var for list of which channels on the multiplexer are connected
+local channel_numbers = {
+  1,
+  2,
+  3,
+  4
+}
 
 -- for each TCA9548A, add an entry with its address
 -- 0x70 is default, to add more set or reset A0, A1, A2
@@ -40,30 +45,31 @@ end
 
 -- MAIN FUNCTION
 function update()
-  -- for i in TCA_ADDRESSES do
-    for j = 1, sensor_ammount do
-      -- select TCA module 1, channel 6
-      -- tcaselect(i, j)
-      tcaselect(1, j)
-      -- once open use the address of the sensor
-      i2c_bus:set_address(sensor_addr)
-      -- read_registers(begin at register, number of bytes to read)
-      returnTable = i2c_bus:read_registers(0, 2)
+  for channel in channel_numbers do
 
-      -- output data to MP Messages
-      -- format data to remove first 2 bits
-      msg = (returnTable[1] << 8 | returnTable[2]) & 0x3FFF
-      -- send_text(priority level (7 is Debug), text as a string formatted to hex)
-      gcs:send_text(7, "Data on " .. "Channel "..string.format("%d", j).. "-> "  .. string.format("%x", msg))
+    -- select TCA module 1, and channel i
+    tcaselect(1, channel)
 
-      -- normalize data to [-2 2] in inH2O and make the datatype string
-      -- math is ((range*data)/max(data) - 2)
-      log_data[i] = tostring((4.0 * msg) / 0x3FFF - 2)
-    end
-  -- end
+    -- once open use the address of the sensor
+    i2c_bus:set_address(sensor_addr)
 
-  logger:write('SENS', 's1,s2,s3,s4,s5,s6', 'NNNNNN', log_data[1], log_data[2], log_data[3], log_data[4], log_data[5],
-    log_data[6])
+    -- read_registers(begin at register, number of bytes to read)
+    returnTable = i2c_bus:read_registers(0, 2)
+
+    -- output data to MP Messages
+    -- format data to remove first 2 bits
+    msg = (returnTable[1] << 8 | returnTable[2]) & 0x3FFF
+
+    -- send_text(priority level (7 is Debug), text as a string formatted to hex)
+    gcs:send_text(7, "Data on " .. "Channel " .. string.format("%d", channel) .. "-> " .. string.format("%x", msg))
+
+    -- normalize data to [-2 2] in inH2O and make the datatype string
+    -- math is ((range*data)/max(data) - 2)
+    log_data[i] = tostring((4.0 * msg) / 0x3FFF - 2)
+  end
+
+  -- logger:write('SENS', 's1,s2,s3,s4,s5,s6', 'NNNNNN', log_data[1], log_data[2], log_data[3], log_data[4], log_data[5], log_data[6])
+  logger:write('SENS','s1,s2,s3,s4','NNNN', log_data[1], log_data[2], log_data[3], log_data[4])
 
   i2c_bus:set_address(0x00)
   return update, 1000 -- reschedules the loop every 1000ms
