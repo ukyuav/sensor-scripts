953a76dd8866e09eceaa3db84b50357e8ce0bc7a
justin-t12 <justin.t12@outlook.com>
2024-05-13 16:36:35 -0400

    started adding error handleing for sensor disconnects. Added temporary code to read a sensor with a different address while new sensors arrive

--- i2c_tca_2.lua
+++ i2c_tca_2.lua
@@ -4,15 +4,18 @@ I2C TCA9548 interface
 
 This program uses the TCA Multiplexer to communicate to multiple devices with the same addresses
 
-***Example included assumes 0x28 is connected channel 6 on the TCA module
-
 Author: Ryan Prince | Last Updated By: Justin Tussey
 Last Updated: 07 May 2024
 
 ]] --
 
--- init i2c bus
+-- list for the log data from the sensors
 local log_data = {}
+
+-- list for errors when reading channels of multiplexer
+local error_list = {}
+
+-- init i2c bus
 local i2c_bus = i2c:get_device(0, 0)
 i2c_bus:set_retries(10)
 gcs:send_text(7, "i2c_tca Script Started!")
@@ -23,7 +26,8 @@ local sensor_addr = 0x28
 -- var for list of which channels on the multiplexer are connected
 local channel_numbers = {
   7,
-  2
+  2,
+  4
 }
 
 -- for each TCA9548A, add an entry with its address
@@ -38,36 +42,50 @@ function tcaselect(tca, channel)
   -- choose multiplexer from array
   i2c_bus:set_address(TCA_ADDRESSES[tca])
   -- set/open the correct channel
-  i2c_bus:write_register(0x70, 1 << channel)
+  -- i2c_bus:write_register(0x70, 1 << channel)
+  i2c_bus:write_register(TCA_ADDRESSES[tca], 1 << channel)
 end
 
 -- MAIN FUNCTION
 function update()
-for key, value in pairs(channel_numbers) do
+  for key, value in pairs(channel_numbers) do
 
     -- select TCA module 1, and channel i
     tcaselect(1, value)
 
     -- once open use the address of the sensor
+    if (value == 4) then
+    i2c_bus:set_address(0x38)
+    else
     i2c_bus:set_address(sensor_addr)
+    end
 
     -- read_registers(begin at register, number of bytes to read)
     returnTable = i2c_bus:read_registers(0, 2)
 
-    -- output data to MP Messages
-    -- format data to remove first 2 bits
-    msg = (returnTable[1] << 8 | returnTable[2]) & 0x3FFF
+    if (returnTable == nil) then
+      gcs:send_text(0, "returnTable val nil," .. " disconn sensor," .. " channel: " .. string.format("%d", value))
+      log_data[key] = tostring(0000)
+      error_list[key] = "ERROR"
+    else
+      -- output data to MP Messages
+      -- format data to remove first 2 bits
+      msg = (returnTable[1] << 8 | returnTable[2]) & 0x3FFF
 
-    -- send_text(priority level (7 is Debug), text as a string formatted to hex)
-    gcs:send_text(7, "Data on " .. "Channel " .. string.format("%d", value) .. "-> " .. string.format("%x", msg))
+      -- normalize data to [-2 2] in inH2O and make the datatype string
+      -- math is ((range*data)/max(data) - 2)
+      normalized_data =  tostring((4.0 * msg) / 0x3FFF - 2)
 
-    -- normalize data to [-2 2] in inH2O and make the datatype string
-    -- math is ((range*data)/max(data) - 2)
-    log_data[key] = tostring((4.0 * msg) / 0x3FFF - 2)
+      -- send_text(priority level (7 is Debug), text as a string formatted to hex)
+      gcs:send_text(7, "Data on " .. "Channel " .. string.format("%d", value) .. "-> " .. string.format("%x", msg) .. " | " .. string.format("%.2f", normalized_data))
+
+      log_data[key] = normalized_data
+      error_list[key] = "NORMAL"
+    end
   end
 
   -- logger:write('SENS', 's1,s2,s3,s4,s5,s6', 'NNNNNN', log_data[1], log_data[2], log_data[3], log_data[4], log_data[5], log_data[6])
-  logger:write('SENS','s1,s2','NN', log_data[1], log_data[2])
+  logger:write('SENS','s1,s2,s3,err1,err2,err3','NNNNNN', log_data[1], log_data[2], log_data[3], error_list[1], error_list[2], error_list[3])
 
   i2c_bus:set_address(0x00)
   return update, 1000 -- reschedules the loop every 1000ms
