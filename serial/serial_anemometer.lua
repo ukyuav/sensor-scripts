58e05a794da359329cd73de7d6bf892139a0c777
justin-t12 <justin.t12@outlook.com>
2024-06-17 16:45:28 -0400

    Anemometer: Updated error messages and removed references to un-parsed message
    Adjusted the ERROR_LIST table to make sure that the error messages are
    16 bytes long or shorter, since the data type we use to log the errors
    has a max of 16 bytes
    
    Also added the word "ANEM" in the error messages that are sent to
    Mission Planner to be clear when running both the Anemometer and Samamet
    since they both have similar messages.
    
    Removed some of the calls to the wind temperature message since we are
    currently not logging it.
    
    Added some better comments to functions and other parts of the code.

--- serial/serial_anemometer.lua
+++ serial/serial_anemometer.lua
@@ -23,15 +23,18 @@ local MAX_MESSAGE_LENGTH = 31
 -- Return rate and calculations
 local SCHEDULE_RATE = 100 --milliseconds
 local TIME_BETWEEN_DATA = 512 --milliseconds
--- number of how many loops we need for us to properly flag that the sensor is not sending data (// is floor division (removes decimal))
+-- number of how many loops we need for us to properly flag that the sensor is
+-- not sending data (// is floor division (removes decimal))
 local LOOPS_TO_FAIL = (TIME_BETWEEN_DATA // SCHEDULE_RATE) + (1)
 
 -- error type table
+-- must be 16 characters or less
 local ERROR_LIST = {
-  "No data received",      -- 1
-  "Checksum fail",         -- 2
-  "Data parsing fail",     -- 3
-  "Alarm message received" -- 4
+  "No data received", -- 1
+  "Checksum fail",    -- 2
+  "Parsing fail",     -- 3
+  "Invalid frame",    -- 4
+  "Alarm received"    -- 5
 }
 
 -- find the serial first (0) scripting serial port instance
@@ -47,6 +50,28 @@ PORT:set_flow_control(0)
 local loops_since_data_received = 0
 
 
+-- Take in string and verify that it is a valid message frame. Specifically a
+-- message in the NMEA-0183 format, which starts with "$" and ends with <CR><LF>
+-- (DOS line ending)
+---@param message_string string
+---@return boolean
+function verify_valid_frame(message_string)
+  -- get the first character in the message, which should be "$" if valid
+  local head = string.sub(message_string, 1, 1)
+
+  -- get the last two characters in the message, which should be <CR><LF>
+  -- ("\r\n") if valid
+  local tail = string.sub(message_string, #message_string-1, #message_string)
+
+  if (head ~= "$") then
+    return false
+  elseif (tail ~= "\r\n") then
+    return false
+  end
+
+  return true
+end
+
 -- Checks if a message is "useful". Make sure its one of the messages we would
 -- like to parse and log
 ---@param  message_string string
@@ -56,7 +81,7 @@ function is_message_useful(message_string)
 
   if message_type == nil then
     return false
-  elseif message_type == "IIMWV" or message_type == "WIXDR" then
+  elseif message_type == "IIMWV" then
     return true
   end
   return false
@@ -88,6 +113,8 @@ function verify_checksum(message_string)
     return false
   end
 
+  -- Take the string of the checksum, convert it to a number, specifying that its
+  -- a hexadecimal number
   incoming_checksum = tonumber(incoming_checksum, 16)
 
   -- starting value of zeros, which will not effect the first XOR
@@ -107,7 +134,6 @@ function verify_checksum(message_string)
 
   -- check if checksum matches, return true or false
   return (checksum == incoming_checksum)
-
 end
 
 -- Take the data from a verified message string and split up the string, using
@@ -137,15 +163,21 @@ function parse_data(message_string)
     table.insert(data_table, str)
   end
 
+  -- report data to mission planner (optional)
+  gcs:send_text(7, "angle: " ..  tostring(data_table[2]) ..
+                   " speed: " .. tostring(data_table[4]))
+
   if message_type == "IIMWV" then
     return log_wind_speed(data_table)
-  elseif message_type == "WIXDR" then
-    return log_wind_temp(data_table)
+  -- elseif message_type == "WIXDR" then
+  --   return log_wind_temp(data_table)
   end
 
   return true
 end
 
+-- take the input table, and check if it has 6 elements in it, then write that
+-- data to the BIN file, with the appropriate names and lables
 ---@param message_table table
 ---@return boolean
 function log_wind_speed(message_table)
@@ -156,7 +188,7 @@ function log_wind_speed(message_table)
   end
 
   if message_table[6] == "V" then
-    status_message = ERROR_LIST[4]
+    status_message = ERROR_LIST[5]
   end
 
   logger:write('ANEM', 'angle,speed,error',  -- section name and labels
@@ -169,17 +201,19 @@ function log_wind_speed(message_table)
   return true
 end
 
----@param message_table table
----@return boolean
-function log_wind_temp(message_table)
-  if #message_table ~= 4 then
-    return false
-  end
+-- ---@param message_table table
+-- ---@return boolean
+-- function log_wind_temp(message_table)
+--   if #message_table ~= 4 then
+--     return false
+--   end
 
-  return true
-end
+--   return true
+-- end
 
--- called when an error is detected, and writes all zeros to the BIN file
+-- called when an error is detected, and writes all zeros to the BIN file and
+-- writes what kind of error was experienced. error_type parameter must be 16
+-- bytes long or less
 ---@param  error_type string
 function log_error(error_type)
   logger:write('ANEM', 'angle,speed,error',  -- section name and labels
@@ -189,23 +223,26 @@ function log_error(error_type)
                error_type)
 end
 
-
+-- primary loop. Gets message from serial interface, verifies that it is valid,
+-- then logs the data to the autopilots BIN file
+---@return function
+---@return integer
 function update()
-
   local n_bytes = PORT:available()
 
   -- Check if there are any bytes available, if not log an error after the
   -- specified number of failed loops
   if n_bytes <= 0 then
     loops_since_data_received = loops_since_data_received + 1
-    if loops_since_data_received > LOOPS_TO_FAIL then
+    if loops_since_data_received >= LOOPS_TO_FAIL then
       log_error(ERROR_LIST[1])
-      gcs:send_text(0, "ERROR: Disconnected Sensor")
+      gcs:send_text(0, "ERROR ANEM: Disconnected Sensor")
     end
     return update, SCHEDULE_RATE
   end
 
-
+  -- get MAX_MESSAGE_LENGTH bytes from the serial line, and place them into a
+  -- string
   local message_string = PORT:readstring(MAX_MESSAGE_LENGTH)
 
   -- make sure we got a message from the serial line
@@ -213,37 +250,47 @@ function update()
     return update, SCHEDULE_RATE
   end
 
-  -- clear loops since data received since we passed the checks
+  -- clear loops since data received since we passed the checks to verify we got
+  -- a message
   loops_since_data_received = 0
 
-  -- if the first character is not a $, which means our data is not lined up,
-  -- clear the serial queue and return
-  if (string.sub(message_string, 1, 1) ~= "$") then
-    -- read the available
+  -- check if we have a valid data frame, which checks for the NMEA-0183
+  -- sentence start and ending characters. If not valid (which means we do not
+  -- have a complete message) clear the queue to realign messages and return
+  if not (verify_valid_frame(message_string)) then
+    -- Read the available bytes in the queue and do nothing with them.
+    -- We effectively clear the queue
     PORT:readstring(PORT:available():toint())
+    log_error(ERROR_LIST[4])
     return update, SCHEDULE_RATE
   end
 
+  -- check if message is one we want to handle, skip if not
   if not (is_message_useful(message_string)) then
     return update, SCHEDULE_RATE
   end
 
+  -- verify the checksum in the message to check if the data has been corrupted
+  -- log an error if we fail the check
   if not (verify_checksum(message_string)) then
     log_error(ERROR_LIST[2])
-    gcs:send_text(0, "ERROR: Data failed checksum")
-  gcs:send_text(7, message_string)
+    gcs:send_text(0, "ERROR ANEM: Data failed checksum")
+    gcs:send_text(7, message_string)
     return update, SCHEDULE_RATE
   end
 
+  -- parse and log the data, log error if we experience an issue. Most likely
+  -- means that the data was corrupted and was not caught by the checksum
+  -- verification (unlikely)
   if not (parse_data(message_string)) then
     log_error(ERROR_LIST[3])
-    gcs:send_text(0, "ERROR: Failed to parse data")
+    gcs:send_text(0, "ERROR ANEM: Failed to parse data")
     return update, SCHEDULE_RATE
   end
 
-  gcs:send_text(7, message_string)
+  -- gcs:send_text(7, message_string)
+  -- reschedule the update function to run every SCHEDULE_RATE milliseconds
   return update, SCHEDULE_RATE
 end
-
 
 return update()
